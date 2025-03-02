4d4f8c8b0a068cd04583ecda881a2ed018fe9f53
Alexander Vladovsky <alexvladovsky@ukr.net>
2025-03-02 19:23:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-var style = window.getComputedStyle(document.body)
-console.log(style.getPropertyValue('--i'))
+let params = new URLSearchParams(document.location.search);
+let project = params.get("project");
+let total = parseInt(params.get("total"), 10);
+let used = parseInt(params.get("used"), 10);
 
-var content = document.getElementById("progress-content").textContent.slice(0, -1);
+var content = Math.ceil(used * 100 / total);
+document.getElementById("progress-content").textContent = content;
+document.getElementById("project").textContent = project;
+
 if(content <= 50) {
   document.getElementById("progress").classList.add("less")
   console.log("workingg")
